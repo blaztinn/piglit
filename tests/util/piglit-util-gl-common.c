4e24ca94533a21950eb1a2bcc77315b5eea0c204
Blaž Tomažič <blaz.tomazic@gmail.com>
2012-06-21 15:46:40 +0200

    util: Make piglit-util.* files API-independent
    Move gl/gles-dependent code from piglit-util.h and piglit-util.c to
    piglit-util-gl-common.h and piglit-util-gl-common.c respectively.
    
    As a result all includes of piglit-util.h were changed to
    piglit-util-gl-common.h.
    
    Signed-off-by: Blaž Tomažič <blaz.tomazic@gmail.com>

---
+++ tests/util/piglit-util-gl-common.c
@@ -0,0 +1,324 @@
+/*
+ * Copyright (c) The Piglit project 2007
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.  IN NO EVENT SHALL
+ * VA LINUX SYSTEM, IBM AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "piglit-util-gl-common.h"
+
+
+bool piglit_is_gles()
+{
+	const char *version_string = (const char *) glGetString(GL_VERSION);
+	return strncmp("OpenGL ES ", version_string, 10) == 0;
+}
+
+int piglit_get_gl_version()
+{
+	const char *version_string = (const char *) glGetString(GL_VERSION);
+	const char *version_number_string;
+	int scanf_count;
+	int major;
+	int minor;
+
+	/* skip to version number */
+	if (strncmp("OpenGL ES ", version_string, 10) == 0)
+		version_number_string = version_string + 10;
+	else
+		version_number_string = version_string;
+
+	/* Interpret version number */
+	scanf_count = sscanf(version_number_string, "%i.%i", &major, &minor);
+	if (scanf_count != 2) {
+		printf("Unable to interpret GL_VERSION string: %s\n",
+		       version_string);
+		piglit_report_result(PIGLIT_FAIL);
+		exit(1);
+	}
+	return 10*major+minor;
+}
+
+bool piglit_is_extension_supported(const char *name)
+{
+	const char *const extensions =
+		(const char*) glGetString(GL_EXTENSIONS);
+
+	return piglit_is_extension_in_string(extensions, name);
+}
+
+void piglit_require_gl_version(int required_version_times_10)
+{
+	if (piglit_is_gles() ||
+	    piglit_get_gl_version() < required_version_times_10) {
+		printf("Test requires GL version %g\n",
+		       required_version_times_10 / 10.0);
+		piglit_report_result(PIGLIT_SKIP);
+		exit(1);
+	}
+}
+
+void piglit_require_extension(const char *name)
+{
+	if (!piglit_is_extension_supported(name)) {
+		printf("Test requires %s\n", name);
+		piglit_report_result(PIGLIT_SKIP);
+		exit(1);
+	}
+}
+
+void piglit_require_not_extension(const char *name)
+{
+	if (piglit_is_extension_supported(name)) {
+		piglit_report_result(PIGLIT_SKIP);
+		exit(1);
+	}
+}
+
+const char* piglit_get_gl_error_name(GLenum error)
+{
+#define CASE(x) case x: return #x; 
+    switch (error) {
+    CASE(GL_INVALID_ENUM)
+    CASE(GL_INVALID_OPERATION)
+    CASE(GL_INVALID_VALUE)
+    CASE(GL_NO_ERROR)
+    CASE(GL_OUT_OF_MEMORY)
+    /* enums that are not available everywhere */
+#if defined(GL_STACK_OVERFLOW)
+    CASE(GL_STACK_OVERFLOW)
+#endif
+#if defined(GL_STACK_UNDERFLOW)
+    CASE(GL_STACK_UNDERFLOW)
+#endif
+#if defined(GL_INVALID_FRAMEBUFFER_OPERATION)
+    CASE(GL_INVALID_FRAMEBUFFER_OPERATION)
+#endif
+    default:
+        return "(unrecognized error)";
+    }
+#undef CASE
+}
+
+GLboolean
+piglit_check_gl_error_(GLenum expected_error, const char *file, unsigned line)
+{
+	GLenum actual_error;
+
+	actual_error = glGetError();
+	if (actual_error == expected_error) {
+		return GL_TRUE;
+	}
+
+	/*
+	 * If the lookup of the error's name is successful, then print
+	 *     Unexpected GL error: NAME 0xHEX
+	 * Else, print
+	 *     Unexpected GL error: 0xHEX
+	 */
+	printf("Unexpected GL error: %s 0x%x\n",
+               piglit_get_gl_error_name(actual_error), actual_error);
+        printf("(Error at %s:%u)\n", file, line);
+
+	/* Print the expected error, but only if an error was really expected. */
+	if (expected_error != GL_NO_ERROR) {
+		printf("Expected GL error: %s 0x%x\n",
+		piglit_get_gl_error_name(expected_error), expected_error);
+        }
+
+	return GL_FALSE;
+}
+
+void piglit_reset_gl_error(void)
+{
+	while (glGetError() != GL_NO_ERROR) {
+		/* empty */
+	}
+}
+
+/* These texture coordinates should have 1 or -1 in the major axis selecting
+ * the face, and a nearly-1-or-negative-1 value in the other two coordinates
+ * which will be used to produce the s,t values used to sample that face's
+ * image.
+ */
+GLfloat cube_face_texcoords[6][4][3] = {
+	{ /* GL_TEXTURE_CUBE_MAP_POSITIVE_X */
+		{1.0,  0.99,  0.99},
+		{1.0,  0.99, -0.99},
+		{1.0, -0.99, -0.99},
+		{1.0, -0.99,  0.99},
+	},
+	{ /* GL_TEXTURE_CUBE_MAP_POSITIVE_Y */
+		{-0.99, 1.0, -0.99},
+		{ 0.99, 1.0, -0.99},
+		{ 0.99, 1.0,  0.99},
+		{-0.99, 1.0,  0.99},
+	},
+	{ /* GL_TEXTURE_CUBE_MAP_POSITIVE_Z */
+		{-0.99,  0.99, 1.0},
+		{-0.99, -0.99, 1.0},
+		{ 0.99, -0.99, 1.0},
+		{ 0.99,  0.99, 1.0},
+	},
+	{ /* GL_TEXTURE_CUBE_MAP_NEGATIVE_X */
+		{-1.0,  0.99, -0.99},
+		{-1.0,  0.99,  0.99},
+		{-1.0, -0.99,  0.99},
+		{-1.0, -0.99, -0.99},
+	},
+	{ /* GL_TEXTURE_CUBE_MAP_NEGATIVE_Y */
+		{-0.99, -1.0,  0.99},
+		{-0.99, -1.0, -0.99},
+		{ 0.99, -1.0, -0.99},
+		{ 0.99, -1.0,  0.99},
+	},
+	{ /* GL_TEXTURE_CUBE_MAP_NEGATIVE_Z */
+		{ 0.99,  0.99, -1.0},
+		{-0.99,  0.99, -1.0},
+		{-0.99, -0.99, -1.0},
+		{ 0.99, -0.99, -1.0},
+	},
+};
+
+const char *cube_face_names[6] = {
+	"POSITIVE_X",
+	"POSITIVE_Y",
+	"POSITIVE_Z",
+	"NEGATIVE_X",
+	"NEGATIVE_Y",
+	"NEGATIVE_Z",
+};
+
+const GLenum cube_face_targets[6] = {
+	GL_TEXTURE_CUBE_MAP_POSITIVE_X,
+	GL_TEXTURE_CUBE_MAP_POSITIVE_Y,
+	GL_TEXTURE_CUBE_MAP_POSITIVE_Z,
+	GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+	GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+	GL_TEXTURE_CUBE_MAP_NEGATIVE_Z,
+};
+
+float piglit_tolerance[4] = { 0.01, 0.01, 0.01, 0.01 };
+
+void
+piglit_set_tolerance_for_bits(int rbits, int gbits, int bbits, int abits)
+{
+	int bits[4] = {rbits, gbits, bbits, abits};
+	int i;
+
+	for (i = 0; i < 4; i++) {
+		if (bits[i] < 2) {
+			/* Don't try to validate channels when there's only 1
+			 * bit of precision (or none).
+			 */
+			piglit_tolerance[i] = 1.0;
+		} else {
+			piglit_tolerance[i] = 3.0 / (1 << bits[i]);
+		}
+	}
+}
+
+typedef union { GLfloat f; GLint i; } fi_type;
+
+/**
+ * Convert a 4-byte float to a 2-byte half float.
+ * Based on code from:
+ * http://www.opengl.org/discussion_boards/ubb/Forum3/HTML/008786.html
+ *
+ * Taken over from Mesa.
+ */
+unsigned short
+piglit_half_from_float(float val)
+{
+	const fi_type fi = {val};
+	const int flt_m = fi.i & 0x7fffff;
+	const int flt_e = (fi.i >> 23) & 0xff;
+	const int flt_s = (fi.i >> 31) & 0x1;
+	int s, e, m = 0;
+	unsigned short result;
+
+	/* sign bit */
+	s = flt_s;
+
+	/* handle special cases */
+	if ((flt_e == 0) && (flt_m == 0)) {
+		/* zero */
+		/* m = 0; - already set */
+		e = 0;
+	}
+	else if ((flt_e == 0) && (flt_m != 0)) {
+		/* denorm -- denorm float maps to 0 half */
+		/* m = 0; - already set */
+		e = 0;
+	}
+	else if ((flt_e == 0xff) && (flt_m == 0)) {
+		/* infinity */
+		/* m = 0; - already set */
+		e = 31;
+	}
+	else if ((flt_e == 0xff) && (flt_m != 0)) {
+		/* NaN */
+		m = 1;
+		e = 31;
+	}
+	else {
+		/* regular number */
+		const int new_exp = flt_e - 127;
+		if (new_exp < -24) {
+			/* this maps to 0 */
+			/* m = 0; - already set */
+			e = 0;
+		}
+		else if (new_exp < -14) {
+			/* this maps to a denorm */
+			/* 2^-exp_val*/
+			unsigned int exp_val = (unsigned int) (-14 - new_exp);
+
+			e = 0;
+			switch (exp_val) {
+			case 0:
+				/* m = 0; - already set */
+				break;
+			case 1: m = 512 + (flt_m >> 14); break;
+			case 2: m = 256 + (flt_m >> 15); break;
+			case 3: m = 128 + (flt_m >> 16); break;
+			case 4: m = 64 + (flt_m >> 17); break;
+			case 5: m = 32 + (flt_m >> 18); break;
+			case 6: m = 16 + (flt_m >> 19); break;
+			case 7: m = 8 + (flt_m >> 20); break;
+			case 8: m = 4 + (flt_m >> 21); break;
+			case 9: m = 2 + (flt_m >> 22); break;
+			case 10: m = 1; break;
+			}
+		}
+		else if (new_exp > 15) {
+			/* map this value to infinity */
+			/* m = 0; - already set */
+			e = 31;
+		}
+		else {
+			/* regular */
+			e = new_exp + 15;
+			m = flt_m >> 13;
+		}
+	}
+
+	result = (s << 15) | (e << 10) | m;
+	return result;
+}
