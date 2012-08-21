5e29dca6e218ea5b8740a56b0ececa847b81e32b
Blaž Tomažič <blaz.tomazic@gmail.com>
2012-08-21 12:49:35 +0200

    cl: Add doc and template files

---
+++ tests/cl/template_api.c
@@ -0,0 +1,16 @@
+#include "piglit-framework-cl-api.h"
+
+PIGLIT_CL_API_TEST_CONFIG_BEGIN
+
+PIGLIT_CL_API_TEST_CONFIG_END
+
+enum piglit_result
+piglit_cl_test(const int argc,
+               const char** argv,
+               const struct piglit_cl_api_test_config* config,
+               const struct piglit_cl_api_test_env* env)
+{
+	enum piglit_result result = PIGLIT_PASS;
+
+	return result;
+}
---
+++ tests/cl/template_program.c
@@ -0,0 +1,16 @@
+#include "piglit-framework-cl-program.h"
+
+PIGLIT_CL_PROGRAM_TEST_CONFIG_BEGIN
+
+PIGLIT_CL_PROGRAM_TEST_CONFIG_END
+
+enum piglit_result
+piglit_cl_test(const int argc,
+               const char** argv,
+               const struct piglit_cl_program_test_config* config,
+               const struct piglit_cl_program_test_env* env)
+{
+	enum piglit_result result = PIGLIT_PASS;
+
+	return result;
+}
